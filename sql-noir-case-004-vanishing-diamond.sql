026e668dae0d41f080350715725d9ed5dfc22b80
andylikesyarn <stowersandy3@gmail.com>
2025-06-10 21:41:17 -0400

    Update sql-noir-case-004-vanishing-diamond.sql

--- sql-noir-case-004-vanishing-diamond.sql
+++ sql-noir-case-004-vanishing-diamond.sql
@@ -1,4 +1,48 @@
--- Record your SQL detective process here!  Write down: 
-  -- 1. The SQL queries you ran
-  -- 2. Any notes or insights as SQL comments
-  -- 3. Your final conclusion: who did it?
+--At Miami’s prestigious Fontainebleau Hotel charity gala, the famous “Heart of Atlantis” diamond necklace suddenly disappeared from its display.
+
+--Fontainebleau hotel
+--theft
+--diamond
+--heart of atlantis
+
+--cool so i'm gonna look at the datasets and try to filter for anything interesting
+
+SELECT * FROM
+attire_registry;
+
+
+SELECT * FROM
+attire_registry
+WHERE note LIKE '%iamond necklace%';
+
+--only one person seen in a diamond necklace
+
+SELECT * FROM
+guest
+JOIN attire_registry
+ON guest.id = attire_registry.guest_id
+WHERE note LIKE '%iamond necklace%';
+
+--ig this is a red herring
+
+--ok let's look at interviews and confessions next
+
+SELECT * FROM
+final_interviews;
+
+--nearly all have 'kill' and are past tense, so let's maybe put in 'stole' or 'took' to see if that works?
+
+
+SELECT * FROM
+final_interviews
+WHERE confession LIKE '%took%'
+OR confession LIKE '%stole%'
+
+--only 1 result w/ id 105
+
+SELECT * FROM guest
+WHERE id=105;
+
+--MIKE MAIN
+
+--ok, hilariously I did NOT do this how the game said to but it still worked.
