f9f7e81989035ec545aff2605253333016b82233
andylikesyarn <stowersandy3@gmail.com>
2025-06-27 17:28:54 -0400

    Update murder-mystery-game.sql

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -53,8 +53,36 @@ WHERE (person_id=14887
 
 --51739	Tushar Chandra	664760	312	Phi St	137882671	664760	21	71	black	black	male	4H42WR	Nissan	Altima
 --67318	Jeremy Bowers	423327	530	Washington Pl, Apt 3A	871539279	423327	30	70	brown	brown	male	0H42W2	Chevrolet	Spark LS
+SELECT *
+FROM person
+JOIN drivers_license
+  ON person.license_id = drivers_license.id
+JOIN get_fit_now_member
+  ON person.id = get_fit_now_member.person_id
+WHERE plate_number LIKE '%H42W%'
+  AND gender = 'male'
+  AND membership_status = 'gold'
 
+-- now only returns Jeremy Bowers
 
+--now querying for the challenge
+SELECT transcript FROM person
+JOIN interview 
+ON person.id = interview.person_id
+WHERE person.name = 'Jeremy Bowers';
+--I was hired by a woman with a lot of money. 
+--I don't know her name but I know she's around 5'5" (65") or 5'7" (67"). 
+--She has red hair and she drives a Tesla Model S. 
+--SELECT * FROM person 
+JOIN drivers_license
+ON person.license_id = drivers_license.id
+JOIN facebook_event_checkin
+ON person.id = facebook_event_checkin.person_id
+WHERE hair_color = 'red'
+AND car_make = 'Tesla'
+AND car_model = 'Model S'
+AND gender = 'female'
+AND height BETWEEN 64 AND 68
 
-
+--returns only miranda priestly
 
